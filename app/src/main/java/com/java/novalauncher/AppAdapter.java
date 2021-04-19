d7d858e2bea0b78d15a4ed9add36ee377ffa5946
user <km_413@naver.com>
2021-04-19 16:10:02 +0900

    lesson3 - 설치된 앱 가져와서 배치하기

--- app/src/main/java/com/java/novalauncher/AppAdapter.java
+++ app/src/main/java/com/java/novalauncher/AppAdapter.java
@@ -1,12 +1,14 @@
 package com.java.novalauncher;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -50,6 +52,7 @@ public class AppAdapter extends BaseAdapter {
             v = convertView;
         }
 
+        LinearLayout mLayout = v.findViewById(R.id.layout);
         ImageView mImage = v.findViewById(R.id.image);
         TextView mLabel = v.findViewById(R.id.appLabel);
 
@@ -57,6 +60,16 @@ public class AppAdapter extends BaseAdapter {
         mImage.setImageDrawable(appList.get(position).getImage());
         mLabel.setText(appList.get(position).getName());
 
+        // 레이아웃 클릭하면 레이아웃을 가져와서 Intent 시켜줌
+        mLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent launchAppIntent = context.getPackageManager().getLaunchIntentForPackage(appList.get(position).getPackageName()); // 클릭한 packageName을 Intent에 넘겨줌
+                if(launchAppIntent != null){
+                    context.startActivity(launchAppIntent);
+                }
+            }
+        });
         //
         return v;
     }
--- app/src/main/java/com/java/novalauncher/MainActivity.java
+++ app/src/main/java/com/java/novalauncher/MainActivity.java
@@ -2,6 +2,9 @@ package com.java.novalauncher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ResolveInfo;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Adapter;
@@ -22,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         initializeDrawer();
     }
 
-    List<AppObject> appList = new ArrayList<>();
+    List<AppObject> installedAppList = new ArrayList<>();
 
     private void initializeDrawer() {
         View mBottomSheet = findViewById(R.id.bottomSheet);
@@ -30,9 +33,43 @@ public class MainActivity extends AppCompatActivity {
         BottomSheetBehavior mBottomSheetBehavior = BottomSheetBehavior.from(mBottomSheet);
         mBottomSheetBehavior.setHideable(false);
         mBottomSheetBehavior.setPeekHeight(300);
-        for(int i = 0; i < 20; i++)
-            appList.add(new AppObject("",String.valueOf(i),getResources().getDrawable(R.drawable.ic_launcher_foreground)));
+        installedAppList = getInstalledAppList();
+        mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), installedAppList));
+    }
 
-        mDrawerGridView.setAdapter(new AppAdapter(getApplicationContext(), appList));
+    private List<AppObject> getInstalledAppList() {
+        List<AppObject> list = new ArrayList<>();
+        // Info about Action MAIN and Category Launcher in Android Manifest
+        // https://stackoverflow.com/questions/6730982/info-about-action-main-and-category-launcher-in-android-manifest
+
+        // https://developer.android.com/reference/android/content/Intent#ACTION_MAIN
+        // Start as a main entry point, does not expect to receive data.
+        // 메인 시작점으로 만듬, 데이터 받지 않음.
+        Intent intent = new Intent(Intent.ACTION_MAIN, null);
+        // App type 지정 . category는 Intent 수행에 있어서 추가적인 디테일을 제공함.
+
+        // https://developer.android.com/reference/android/content/Intent#addCategory(java.lang.String)
+        intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        // https://developer.android.com/reference/android/content/pm/PackageManager
+        // 아직 처리되지 않은 앱 리스트를 받음.
+        // ApplicationContext - Application lifeCycle에 묶여있는 Context
+        // getPackageManager - Class for retrieving various kinds of information related to the application packages that are currently installed on the device.
+        // queryIntentActivity - Retrieve all activities that can be performed for the given intent. /
+        List<ResolveInfo> untreatedAppList = getApplicationContext()
+                .getPackageManager()
+                .queryIntentActivities(intent, 0); // flag 0 - INSTALL_SCENARIO_DEFAULT : A value to indicate the lack of CUJ information, disabling all installation scenario logic.
+
+        // 객체 생성 후 리스트에 추가
+        for(ResolveInfo UntreatedApp : untreatedAppList){
+            String appName = UntreatedApp.activityInfo.loadLabel(getPackageManager()).toString();
+            String appPackageName = UntreatedApp.activityInfo.packageName;
+            Drawable appImage = UntreatedApp.activityInfo.loadIcon(getPackageManager());
+            AppObject app = new AppObject(appPackageName, appName, appImage);
+
+            if(!list.contains(app))
+                list.add(app);
+        }
+        return list;
     }
 }
